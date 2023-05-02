058b99addb145a9f83d83606ba8b77830cce9f7f
J. Gabriel González E. <jesusgabriel@192.168.1.67>
2023-05-02 13:13:20 -0600

    Ventanas hechas 1mayo

--- src/ProyectoFinal/controladorPanel.java
+++ src/ProyectoFinal/controladorPanel.java
@@ -1,29 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ProyectoFinal;
 
 import javax.swing.JPanel;
 
-/**
- *
- * @author jesusgabriel
- */
-
     public abstract class controladorPanel extends JPanel {
         
-        
-            private boolean x=true;
-            public void mostrar(){
-            this.setVisible(x);
-            this.setEnabled(x);
-            }
-            
-            public void ocultar(){
-            this.setVisible(!x);
-            this.setEnabled(!x);
-            }
+        protected frmMenu menu;
+     
+   
+       public controladorPanel(frmMenu menu) {
+        this.menu = menu;
+        }
+
+        public controladorPanel() {
+        }
+       
         
         }
 
